b8c25915e418ceb79abe8a144cf09fc6bf9e8806
rohe0002 <roland.hedberg@adm.umu.se>
2011-11-02 14:12:02 +0100

    Added consumer and server to OIC.

---
+++ src/oic/oic/consumer.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+
+__author__ = 'rohe0002'
+
+import random
+import string
+import time
+
+from hashlib import md5
+
+from oic.utils import http_util
+from oic.oauth2 import AuthorizationRequest
+from oic.oauth2 import Client
+from oic.oauth2 import ErrorResponse
+
+def stateID(url, seed):
+    """The hash of the time + server path + a seed makes an unique
+    SID for each session.
+
+    :param url: The base URL for this site
+    :return: The hex version of the digest
+    """
+    ident = md5()
+    ident.update(repr(time.time()))
+    ident.update(url)
+    ident.update(seed)
+    return ident.hexdigest()
+
+def rndstr(size=16):
+    """
+    Returns a string of random characters
+
+    :param size: The length of the string
+    :return: string
+    """
+    return "".join([random.choice(string.ascii_letters) for _ in range(size)])
+
+def factory(kaka, sdb, config):
+    """
+    Return the right Consumer instance dependent on what's in the cookie
+
+    :param kaka: The cookie
+    :param sdb: The session database
+    :param config: The common Consumer configuration
+    :return: Consumer instance or None
+    """
+    part = http_util.cookie_parts(config["name"], kaka)
+    if part is None:
+        return None
+
+    cons = Consumer(sdb, config=config)
+    cons.restore(part[0])
+    http_util.parse_cookie(config["name"], cons.seed, kaka)
+    return cons
+
+class UnknownState(Exception):
+    pass
+
+class TokenError(Exception):
+    pass
+
+class AuthzError(Exception):
+    pass
+
+class Consumer(Client):
+    """ An OpenID Connect consumer implementation
+
+    """
+    #noinspection PyUnusedLocal
+    def __init__(self, session_db, config, client_config=None,
+                 server_info=None):
+        """ Initializes a Consumer instance.
+
+        :param session_db: Where info are kept about sessions
+        :param config: Configuration of the consumer
+        :param client_config: Client configuration
+        :param server_info: Information about the server
+        """
+        if client_config is None:
+            client_config = {}
+
+        Client.__init__(self, **client_config)
+
+        self.config = config
+        if config:
+            self.debug = config["debug"]
+
+        if server_info:
+            self.authorization_endpoint = server_info["authorization_endpoint"]
+            self.token_endpoint = server_info["token_endpoint"]
+
+        self.sdb = session_db
+        self.seed = ""
+
+    def restore(self, sid):
+        """ Restores the instance variables from something stored in the
+        session database.
+
+        :param sid: Session identifier
+        """
+        for key, val in self.sdb[sid].items():
+            setattr(self, key, val)
+
+    def _backup(self, sid):
+        """ Stores instance variable values in the session store under a
+        session identifier.
+
+        :param sid: Session identifier
+        """
+        self.sdb[sid] = {
+            "client_id": self.client_id,
+            "state": self.state,
+            "authorization_code": self.authorization_code,
+            "grant_expiration_time": self.grant_expiration_time,
+            "scope": self.scope,
+            "access_token": self.access_token,
+            "token_expiration_time": self.token_expiration_time,
+            "redirect_uri": self.redirect_uri,
+            "authorization_endpoint": self.authorization_endpoint,
+            "token_endpoint": self.token_endpoint,
+            "token_revocation_endpoint": self.token_revocation_endpoint,
+            "seed": self.seed,
+            "debug": self.debug,
+        }
+
+    #noinspection PyUnusedLocal,PyArgumentEqualDefault
+    def begin(self, environ, start_response, logger):
+        """ Begin the OAuth2 flow
+
+        :param environ: The WSGI environment
+        :param start_response: The function to start the response process
+        :param logger: A logger instance
+        :return: A URL to which the user should be redirected
+        """
+        _log_info = logger.info
+
+        if self.debug:
+            _log_info("- begin -")
+
+        _path = http_util.geturl(environ, False, False)
+        self.redirect_uri = _path + self.config["authz_page"]
+
+        # Put myself in the dictionary of sessions, keyed on session-id
+        if not self.seed:
+            self.seed = rndstr()
+
+        sid = stateID(_path, self.seed)
+        self._backup(sid)
+        self.sdb["seed:%s" % self.seed] = sid
+
+        # Store the request and the redirect uri used
+        self._request = http_util.geturl(environ)
+
+        areq = self.get_authorization_request(AuthorizationRequest,
+                            state=sid,
+                            response_type=self.config["response_type"],
+                            scope=self.config["scope"])
+
+        location = "%s?%s" % (self.authorization_endpoint,
+                              areq.get_urlencoded())
+
+        if self.debug:
+            _log_info("Redirecting to: %s" % location)
+
+        return location
+
+    #noinspection PyUnusedLocal
+    def parse_authz(self, environ, start_response, logger):
+        """
+        This is where we get redirect back to after authorization at the
+        authorization server has happened.
+
+        :param environ: The WSGI environment
+        :param start_response: The function to start the response process
+        :param logger: A logger instance
+        :return: A AccessTokenResponse instance
+        """
+
+        _log_info = logger.info
+        if self.debug:
+            _log_info("- authorization -")
+            _log_info("- %s flow -" % self.config["flow_type"])
+
+        _query = environ.get("QUERY_STRING")
+        _path = http_util.geturl(environ, False, False)
+
+        if self.config["flow_type"] == "code":
+            # Might be an error response
+            aresp = self.parse_authorization_response(query=_query)
+            if isinstance(aresp, ErrorResponse):
+                raise AuthzError(aresp.error)
+
+            try:
+                self.restore(aresp.state)
+            except KeyError:
+                raise UnknownState(aresp.state)
+
+            self.set_from_authorization_response(aresp)
+            self._backup(aresp.state)
+            return aresp
+        else:
+            atr = self.parse_access_token_response(info=_query,
+                                                   format="urlencoded",
+                                                   extended=True)
+            if isinstance(atr, ErrorResponse):
+                raise TokenError(atr.error)
+
+            return atr
+
+    def complete(self, logger):
+        """
+        Do the access token request, the last step in a code flow.
+        If Implicit flow was used then this method is never used.
+        """
+        if self.config["password"]:
+            logger.info("basic auth")
+            atr = self.do_access_token_request(code=self.authorization_code,
+                                    grant_type="authorization_code",
+                                    client_password=self.config["password"])
+        elif self.config["client_secret"]:
+            logger.info("request_body auth")
+            atr = self.do_access_token_request(code=self.authorization_code,
+                                    grant_type="authorization_code",
+                                    auth_method="request_body",
+                                    client_secret=self.config["client_secret"])
+        else:
+            raise Exception("Nothing to authenticate with")
+
+        if isinstance(atr, ErrorResponse):
+            raise TokenError(atr.error)
+
+        #self._backup(self.sdb["seed:%s" % _cli.seed])
+        self._backup(self.state)
+
+        return atr
+
+
+    #noinspection PyUnusedLocal
+    def userinfo(self, logger):
+        pass
+
+    def refresh_session(self):
+        pass
+
+    def check_session(self):
+        pass
+
+    def end_session(self):
+        pass
+    
---
+++ src/oic/oic/server.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+
+__author__ = 'rohe0002'
+
+import random
+import string
+
+try:
+    from urlparse import parse_qs
+except ImportError:
+    from cgi import parse_qs
+
+from oic.utils.http_util import *
+from oic.oauth2 import MissingRequiredAttribute
+
+from oic.oauth2 import AuthorizationResponse
+from oic.oauth2 import AuthorizationRequest
+from oic.oauth2 import AccessTokenResponse
+from oic.oauth2 import AccessTokenRequest
+from oic.oauth2 import TokenErrorResponse
+from oic import oauth2
+
+class AuthnFailure(Exception):
+    pass
+
+#noinspection PyUnusedLocal
+def devnull(txt):
+    pass
+
+def get_post(environ):
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+      request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except ValueError:
+      request_body_size = 0
+
+    # When the method is POST the query string will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    return environ['wsgi.input'].read(request_body_size)
+
+#noinspection PyUnusedLocal
+def do_authorization(user):
+    return ""
+
+def rndstr(size=16):
+    return "".join([random.choice(string.ascii_letters) for _ in range(size)])
+
+class Server(oauth2.Server):
+
+    def __init__(self, name, sdb, cdb, authn_func, authz_func, verify_func,
+                 verify_client, urlmap=None, debug=0):
+        self.name = name
+        self.sdb = sdb
+        self.cdb = cdb
+
+        self.authn_func = authn_func
+        self.verify_func = verify_func
+        self.authz_func = authz_func
+        self.verify_client = verify_client
+
+        self.debug = debug
+        self.seed = rndstr()
+        if urlmap is None:
+            self.urlmap = {}
+        else:
+            self.urlmap = urlmap
+
+    #noinspection PyUnusedLocal
+    def authenticated(self, environ, start_response, logger, _):
+        """
+        After the authentication this is where you should end up
+        """
+
+        _log_info = logger.info
+        _sdb = self.sdb
+
+        if self.debug:
+            _log_info("- authenticated -")
+
+        # parse the form
+        dic = parse_qs(get_post(environ))
+
+        try:
+            (verified, user) = self.verify_func(dic)
+            if not verified:
+                resp = Unauthorized("Wrong password")
+                return resp(environ, start_response)
+        except AuthnFailure, err:
+            resp = Unauthorized("%s" % (err,))
+            return resp(environ, start_response)
+
+        try:
+            # Use the session identifier to find the session information
+            scode = dic["sid"][0]
+            asession = _sdb[scode]
+        except KeyError:
+            resp = BadRequest("")
+            return resp(environ, start_response)
+
+        _sdb.update(scode, "userid", dic["login"][0])
+
+        if self.debug:
+            _log_info("asession[\"authzreq\"] = %s" % asession["authzreq"])
+        #_log_info( "type: %s" % type(asession["authzreq"]))
+
+        # pick up the original request
+        areq = AuthorizationRequest.set_json(asession["authzreq"], extended=True)
+
+        if self.debug:
+            _log_info("areq: %s" % areq)
+
+        # Do the authorization
+        try:
+            permission = self.authz_func(user)
+            _sdb.update(scode, "permission", permission)
+        except Exception:
+            raise
+
+        _log_info("response type: %s" % areq.response_type)
+
+        # create the response
+        if "code" in areq.response_type:
+            aresp = AuthorizationResponse()
+            if areq.state:
+                aresp.state = areq.state
+
+            if self.debug:
+                _log_info("_dic: %s" % _sdb[scode])
+            aresp.code = scode
+            aresp.c_extension = areq.c_extension
+        elif "token" in areq.response_type:
+            _dic = _sdb.update_to_token(scode, issue_refresh=False)
+            if self.debug:
+                _log_info("_dic: %s" % _dic)
+            aresp = oauth2.factory(AccessTokenResponse, **_dic)
+
+            if areq.state:
+                aresp.state = areq.state
+            if areq.scope:
+                aresp.scope = areq.scope
+            aresp.c_extension = areq.c_extension
+
+        elif "none" in areq.response_type:
+            # return only state
+
+            aresp = AuthorizationResponse()
+            if areq.state:
+                aresp.state = areq.state
+        else: # Don't know what to do raise an exception
+            resp = BadRequest("Unknown response type")
+            return resp(environ, start_response)
+
+        if areq.redirect_uri:
+            # TODO verify that the uri is reasonable
+            redirect_uri = areq.redirect_uri
+        else:
+            redirect_uri = self.urlmap[areq.client_id]
+
+        location = "%s?%s" % (redirect_uri, aresp.get_urlencoded())
+
+        if self.debug:
+            _log_info("Redirected to: '%s' (%s)" % (location, type(location)))
+
+        redirect = Redirect(str(location))
+        return redirect(environ, start_response)
+
+    #noinspection PyUnusedLocal
+    def authorization_endpoint(self, environ, start_response, logger, _):
+        # The AuthorizationRequest endpoint
+
+        _log_info = logger.info
+        _sdb = self.sdb
+
+        if self.debug:
+            _log_info("- authorization -")
+
+        if environ.get("REQUEST_METHOD") == "GET":
+            query = environ.get("QUERY_STRING")
+        elif environ.get("REQUEST_METHOD") == "POST":
+            query = get_post(environ)
+        else:
+            resp = BadRequest("Unsupported method")
+            return resp(environ, start_response)
+
+        if self.debug:
+            _log_info("Query: '%s'" % query)
+
+        try:
+            areq = self.parse_authorization_request(query=query,
+                                                    extended=True)
+        except MissingRequiredAttribute, err:
+            resp = BadRequest("%s" % err)
+            return resp(environ, start_response)
+        except Exception,err:
+            resp = BadRequest("%s" % err)
+            return resp(environ, start_response)
+
+        if areq.redirect_uri:
+            _redirect = areq.redirect_uri
+        else:
+            # A list, so pick one (==the first)
+            _redirect = self.urlmap[areq.client_id][0]
+
+        sid = _sdb.create_authz_session("", areq)
+        grant = _sdb[sid]["code"]
+        if self.debug:
+            _log_info("code: '%s'" % grant)
+
+        return self.authn_func(environ, start_response, grant)
+
+    #noinspection PyUnusedLocal
+    def token_endpoint(self, environ, start_response, logger, handle):
+        """
+        This is where clients come to get their access tokens
+        """
+
+        _log_info = logger.info
+        _sdb = self.sdb
+
+        if self.debug:
+            _log_info("- token -")
+        body = get_post(environ)
+        if self.debug:
+            _log_info("body: %s" % body)
+
+        areq = AccessTokenRequest.set_urlencoded(body, extended=True)
+
+        if not self.verify_client(environ, areq, self.cdb):
+            err = TokenErrorResponse(error="unathorized_client")
+            resp = Response(err.get_json, content="application/json")
+            return resp(environ, start_response)
+
+        if self.debug:
+            _log_info("AccessTokenRequest: %s" % areq)
+
+        assert areq.grant_type == "authorization_code"
+
+        # assert that the code is valid
+        _info = _sdb[areq.code]
+
+        # If redirect_uri was in the initial authorization request
+        # verify that the one given here is the correct one.
+        if "redirect_uri" in _info:
+            assert areq.redirect_uri == _info["redirect_uri"]
+
+        _tinfo = _sdb.update_to_token(areq.code)
+
+        if self.debug:
+            _log_info("_tinfo: %s" % _tinfo)
+
+        atr = oauth2.factory(AccessTokenResponse, **_tinfo)
+
+        if self.debug:
+            _log_info("AccessTokenResponse: %s" % atr)
+
+        resp = Response(atr.get_json(), content="application/json")
+        return resp(environ, start_response)
+
+  
