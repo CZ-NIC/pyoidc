55315036e57482caef13ef7adb8d9efcbba6dc48
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-02-24 17:08:33 +0100

    Oauth2 Authorization Server

---
+++ as/config.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+__author__ = 'rolandh'
+
+baseurl = "https://localhost"
+issuer = "%s:8092" % baseurl
+
+COOKIENAME = 'pyoic'
+COOKIETTL = 4 * 60  # 4 hours
+SEED = "SoLittleTime,GotToHurry"
+SERVER_CERT = "certs/server.crt"
+SERVER_KEY = "certs/server.key"
+#CERT_CHAIN="certs/chain.pem"
+CERT_CHAIN = None
+
+
+USERDB = {
+    "diana": {
+        "user_id": "dikr0001",
+        "name": "Diana Krall",
+        "given_name": "Diana",
+        "family_name": "Krall",
+        "nickname": "Dina",
+        "email": "diana@example.org",
+        "email_verified": False,
+        "phone_number": "+46 90 7865000",
+        "address": {
+            "street_address": "Umeå Universitet",
+            "locality": "Umeå",
+            "postal_code": "SE-90187",
+            "country": "Sweden"
+        },
+    },
+    "babs": {
+        "user_id": "babs0001",
+        "name": "Barbara J Jensen",
+        "given_name": "Barbara",
+        "family_name": "Jensen",
+        "nickname": "babs",
+        "email": "babs@example.com",
+        "email_verified": True,
+        "address": {
+            "street_address": "100 Universal City Plaza",
+            "locality": "Hollywood",
+            "region": "CA",
+            "postal_code": "91608",
+            "country": "USA",
+        },
+    },
+    "upper": {
+        "user_id": "uppe0001",
+        "name": "Upper Crust",
+        "given_name": "Upper",
+        "family_name": "Crust",
+        "email": "uc@example.com",
+        "email_verified": True,
+    }
+}
+
+CLIENT = {
+    "1234567890" :{
+        "client_secret": "SoonerOrLater",
+        "return_uris": ["https://localhost:8091"]
+    }
+}
---
+++ as/templates/login.mako
@@ -0,0 +1,38 @@
+<%inherit file="root.mako" />
+<%def name="title()">Log in</%def>
+
+<div class="login_form" class="block">
+    <form action="${action}" method="post" class="login form">
+        <input type="hidden" name="sid" value="${sid}"/>
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="login" value="${login}"/></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password"
+                           value="${password}"/></td>
+            </tr>
+            <tr>
+                </td>
+                <td><input type="submit" name="form.commit"
+                           value="Log In"/></td>
+            </tr>
+        </table>
+    </form>
+    % if logo_url:
+            <img src="${logo_url}" alt="Client logo">
+    % endif
+    % if policy_url:
+            <a href="${policy_url}"><b>Client policy</b></a>
+    % endif
+</div>
+
+<%def name="add_js()">
+    <script type="text/javascript">
+        $(document).ready(function() {
+            bookie.login.init();
+        });
+    </script>
+</%def>
