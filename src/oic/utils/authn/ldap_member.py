b3973e95ef1ec22bc0d629a757c6780ceb50c3c7
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-05-01 09:15:12 +0200

    To use the same method format as the super class.

--- src/oic/utils/authn/ldap_member.py
+++ src/oic/utils/authn/ldap_member.py
@@ -10,14 +10,12 @@ class UserLDAPMemberValidation(UserInfoLDAP):
     CONST_LDAPMEMBER = 'eduPersonScopedAffiliation;x-guise-anst2'
     CONST_VALIDFIELDS = ['employee@umu.se', 'staff@umu.se', 'member@umu.se']
 
-
-
     def __init__(self,ldap_member, valid_fields, **kwargs):
         UserInfoLDAP.__init__(self, **kwargs)
         self.ldap_member = ldap_member
         self.valid_fields = valid_fields
 
-    def __call__(self, userid):
+    def __call__(self, userid, **kwargs):
         result = UserInfoLDAP.__call__(self, userid)
         if self.ldap_member in result:
             for field in result[self.ldap_member]:
