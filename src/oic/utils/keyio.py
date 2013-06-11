08652931bd6780604cc4f361ab4834db4a11cf05
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-06-11 09:18:42 +0200

    Move class definition of RedirectStdStreams from pyjwkest to here.

--- src/oic/utils/keyio.py
+++ src/oic/utils/keyio.py
@@ -1,5 +1,4 @@
 import json
-from jwkest.keyio import RedirectStdStreams
 
 __author__ = 'rohe0002'
 
@@ -481,6 +480,22 @@ class KeyJar(object):
 
 
 # =============================================================================
+
+class RedirectStdStreams(object):
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush(); self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    #noinspection PyUnusedLocal
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush(); self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
 
 
 def key_setup(vault, **kwargs):
