3dd11b2c00e802d1cb3f90345a6b2eb8742c5454
Hans Hörberg <hans.horberg@umu.se>
2013-09-12 16:01:00 +0200

    RP implementation för pyoidc

---
+++ rp/openidconnect/__init__.py
@@ -0,0 +1,257 @@
+import uuid
+
+__author__ = 'haho0032'
+import copy
+import traceback
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+from rp import Social
+from oic import oic
+#from oic.oic import consumer
+from oic.oauth2 import rndstr
+from oic.oauth2.message import ErrorResponse
+
+from oic.oic.message import AuthorizationResponse
+from oic.oic.message import AuthorizationRequest
+from oic.oic.message import AccessTokenResponse
+
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def token_secret_key(sid):
+    return "token_secret_%s" % sid
+
+SERVICE_NAME = "OIC"
+#CLIENT_REDIRECT_URIS = ["http://lingon.catalogix.se:8091/oic"]
+FLOW_TYPE = "code" # or "token"
+
+CLIENT_CONFIG = {}
+
+ME = {
+    "application_type": "web",
+    "application_name": "idpproxy",
+    "contacts": ["ops@example.com"],
+}
+
+
+class OpenIDConnect(Social):
+    def __init__(self, client_id, client_secret, **kwargs):
+        Social.__init__(self, client_id, client_secret, **kwargs)
+        try:
+            self.srv_discovery_url = kwargs["srv_discovery_url"]
+        except KeyError:
+            self.srv_discovery_url = None
+        self.flow_type = FLOW_TYPE
+        self.access_token_response = AccessTokenResponse
+        self.client_cls = oic.Client
+        self.authn_method = None
+
+    def dynamic(self, server_env, callback, session):
+        try:
+            client = server_env["OIC_CLIENT"][self.opKey]
+        except KeyError:
+            client = self.client_cls(client_authn_method=CLIENT_AUTHN_METHOD)
+            client.redirect_uris = [callback]
+            _me = ME.copy()
+            _me["redirect_uris"] = [callback]
+
+            provider_conf = client.provider_config(self.srv_discovery_url)
+            logger.debug("Got provider config: %s" % provider_conf)
+            session.setService(provider_conf["issuer"])
+            logger.debug("Registering RP")
+            reg_info = client.register(provider_conf["registration_endpoint"],
+                                       **_me)
+            logger.debug("Registration response: %s" % reg_info)
+            for prop in ["client_id", "client_secret"]:
+                try:
+                    setattr(client, prop, reg_info[prop])
+                except KeyError:
+                    pass
+            try:
+                server_env["OIC_CLIENT"][self.opKey] = client
+            except KeyError:
+                server_env["OIC_CLIENT"] = {self.opKey: client}
+
+        return client
+
+    def static(self, server_env, callback):
+        try:
+            client = server_env["OIC_CLIENT"][self.opKey]
+            logger.debug("Static client: %s" % server_env["OIC_CLIENT"])
+        except KeyError:
+            client = self.client_cls(client_authn_method=CLIENT_AUTHN_METHOD)
+            client.redirect_uris = [callback]
+            for typ in ["authorization", "token", "userinfo"]:
+                endpoint = "%s_endpoint" % typ
+                setattr(client, endpoint, self.extra[endpoint])
+
+            client.client_id = self.client_id
+            client.client_secret = self.client_secret
+
+            if "keys" in self.extra:
+                client.keyjar.add(self.extra["keys"][0],self.extra["keys"][1])
+
+            try:
+                server_env["OIC_CLIENT"][self.opKey] = client
+            except KeyError:
+                server_env["OIC_CLIENT"] = {self.opKey: client}
+        return client
+
+    #noinspection PyUnusedLocal
+    def begin(self, environ, server_env, start_response, session):
+        """Step 1: Get a access grant.
+
+        :param environ:
+        :param start_response:
+        :param server_env:
+        :param cookie:
+        :param sid:
+        :param info:
+        """
+        try:
+            logger.debug("FLOW type: %s" % self.flow_type)
+            logger.debug("begin environ: %s" % server_env)
+
+            callback = server_env["base_url"] + self.opKey
+
+            if self.srv_discovery_url:
+                client = self.dynamic(server_env, callback, session)
+            else:
+                client = self.static(server_env, callback)
+            client.state = session.getState()
+
+            request_args = {
+                "response_type": self.flow_type,
+                "scope": self.extra["scope"],
+                "state":  client.state,
+            }
+
+            if self.flow_type == "token":
+                request_args["nonce"] = rndstr(16)
+                session.setNonce(request_args["nonce"])
+            else:
+                use_nonce = getattr(self, "use_nonce", None)
+                if use_nonce:
+                    request_args["nonce"] = rndstr(16)
+                    session.setNonce(request_args["nonce"])
+
+
+            logger.info("client args: %s" % client.__dict__.items(),)
+            logger.info("request_args: %s" % (request_args,))
+            # User info claims
+        except Exception:
+            message = traceback.format_exception(*sys.exc_info())
+            logger.error(message)
+            return self.result(environ, start_response, server_env,(False, "Cannot find the OP! Please view your configuration of pyoidc RP."))
+
+
+        try:
+            cis = client.construct_AuthorizationRequest(
+                request_args=request_args)
+            logger.debug("request: %s" % cis)
+
+            url, body, ht_args, cis = client.uri_and_body(
+                AuthorizationRequest, cis, method="GET",
+                request_args=request_args)
+            logger.debug("body: %s" % body)
+        except Exception:
+            message = traceback.format_exception(*sys.exc_info())
+            logger.error(message)
+            return self.result(environ, start_response, server_env,(False, "Authorization request can not be performed!"))
+
+        logger.info("URL: %s" % url)
+        logger.debug("ht_args: %s" % ht_args)
+
+        #session.setAuthn_auth(client.authorization_endpoint)
+        #session.setAuthentication("VERIFY")
+
+        #server_env["CACHE"][sid] = session
+        session.setClient(client)
+        resp_headers = [("Location", str(url))]
+        if ht_args:
+            resp_headers.extend([(a, b) for a, b in ht_args.items()])
+        logger.debug("resp_headers: %s" % resp_headers)
+        start_response("302 Found", resp_headers)
+        return []
+
+    def get_accesstoken(self, client, authresp):
+        if self.srv_discovery_url:
+            issuer = client.provider_info.keys()[0]
+            #logger.debug("state: %s (%s)" % (client.state, msg["state"]))
+            key = client.keyjar.get_verify_key(owner=issuer)
+            kwargs = {"key": key}
+            logger.debug("key: %s" % key)
+        else:
+            kwargs = {"keyjar": client.keyjar}
+
+        if self.authn_method:
+            kwargs["authn_method"] = self.authn_method
+
+        # get the access token
+        return client.do_access_token_request(
+            state=authresp["state"], response_cls=self.access_token_response,
+            **kwargs)
+
+    #noinspection PyUnusedLocal
+    def verify_token(self, client, access_token):
+        return {}
+
+    def get_userinfo(self, client, authresp, access_token, **kwargs):
+        # use the access token to get some userinfo
+        return client.do_user_info_request(state=authresp["state"],
+                                           schema="openid",
+                                           access_token=access_token,
+                                           **kwargs)
+
+    #noinspection PyUnusedLocal
+    def phaseN(self, environ, query, server_env, session):
+        """Step 2: Once the consumer has redirected the user back to the
+        callback URL you can request the access token the user has
+        approved."""
+
+        client = session.getClient()
+        logger.debug("info: %s" % query)
+        logger.debug("keyjar: %s" % client.keyjar)
+
+        authresp = client.parse_response(AuthorizationResponse, query,
+                                         sformat="dict")
+
+        if isinstance(authresp, ErrorResponse):
+            return False, "Access denied"
+
+        #session.session_id = msg["state"]
+
+        logger.debug("callback environ: %s" % environ)
+
+        if self.flow_type == "code":
+            # get the access token
+            try:
+                tokenresp = self.get_accesstoken(client, authresp)
+            except Exception, err:
+                logger.error("%s" % err)
+                raise
+
+            if isinstance(tokenresp, ErrorResponse):
+                return (False, "Invalid response %s." % tokenresp["error"])
+
+            access_token = tokenresp["access_token"]
+        else:
+            access_token = authresp["access_token"]
+
+        userinfo = self.verify_token(client, access_token)
+
+        inforesp = self.get_userinfo(client, authresp, access_token)
+
+        if isinstance(inforesp, ErrorResponse):
+            return False, "Invalid response %s." % inforesp["error"], session
+
+        tot_info = userinfo.update(inforesp.to_dict())
+
+        logger.debug("UserInfo: %s" % inforesp)
+
+        return True, userinfo, access_token, client
+
+
+
---
+++ rp/rp_server.py
@@ -0,0 +1,231 @@
+import uuid
+from rp import rp_conf
+from rp.pyoidc import pyoidcOIC
+from cherrypy import wsgiserver
+import logging
+from logging.handlers import BufferingHandler
+from oic.utils.http_util import NotFound
+from saml2.httputil import Response
+from mako.lookup import TemplateLookup
+from urlparse import parse_qs
+from oic.utils.webfinger import URINormalizer
+from oic.utils.webfinger import WebFinger
+from oic.utils.webfinger import OIC_ISSUER
+import requests
+import hashlib
+import base64
+from beaker.middleware import SessionMiddleware  #https://pypi.python.org/pypi/Beaker
+
+__author__ = 'haho0032'
+
+LOGGER = logging.getLogger("")
+LOGFILE_NAME = 'rp.log'
+hdlr = logging.FileHandler(LOGFILE_NAME)
+base_formatter = logging.Formatter(
+    "%(asctime)s %(name)s:%(levelname)s %(message)s")
+
+CPC = ('%(asctime)s %(name)s:%(levelname)s '
+       '[%(client)s,%(path)s,%(cid)s] %(message)s')
+cpc_formatter = logging.Formatter(CPC)
+
+hdlr.setFormatter(base_formatter)
+LOGGER.addHandler(hdlr)
+LOGGER.setLevel(logging.DEBUG)
+
+
+_formatter = logging.Formatter(CPC)
+fil_handl = logging.FileHandler(LOGFILE_NAME)
+fil_handl.setFormatter(_formatter)
+
+buf_handl = BufferingHandler(10000)
+buf_handl.setFormatter(_formatter)
+
+LOOKUP = TemplateLookup(directories=['templates', 'htdocs'],
+                        module_directory='modules',
+                        input_encoding='utf-8',
+                        output_encoding='utf-8')
+
+SERVER_ENV = {}
+
+def setup_server_env(rp_conf):
+    global SERVER_ENV
+    global logger
+    #noinspection PyUnboundLocalVariable
+    SERVER_ENV = dict([(k, v) for k, v in rp_conf.__dict__.items()
+                       if not k.startswith("__")])
+    SERVER_ENV["service"] = rp_conf.SERVICE
+    SERVER_ENV["template_lookup"] = LOOKUP
+    SERVER_ENV["base_url"] = rp_conf.BASE
+    #SERVER_ENV["CACHE"] = {}
+    SERVER_ENV["OIC_CLIENT"] = {}
+
+class Httpd(object):
+    def http_request(self, url):
+        return requests.get(url, verify=False)
+
+class RpSession(object):
+    def __init__(self, session):
+        self.session = session
+        self.getState()
+        self.getService()
+        self.getNonce()
+        self.getClient()
+
+    def getState(self):
+        return self.session.get("state", uuid.uuid4().urn)
+
+    def setState(self, value):
+        self.session["state"] = value
+
+    def getService(self):
+        return self.session.get("service", None)
+
+    def setService(self, value):
+        self.session["service"] = value
+
+    def getNonce(self):
+        return self.session.get("nonce", None)
+
+    def setNonce(self, value):
+        self.session["nonce"] = value
+
+    def getClient(self):
+        return self.session.get("client", None)
+
+    def setClient(self, value):
+        self.session["client"] = value
+
+#noinspection PyUnresolvedReferences
+def static(environ, start_response, logger, path):
+    logger.info("[static]sending: %s" % (path,))
+
+    try:
+        text = open(path).read()
+        if path.endswith(".ico"):
+            start_response('200 OK', [('Content-Type', "image/x-icon")])
+        elif path.endswith(".html"):
+            start_response('200 OK', [('Content-Type', 'text/html')])
+        elif path.endswith(".json"):
+            start_response('200 OK', [('Content-Type', 'application/json')])
+        elif path.endswith(".txt"):
+            start_response('200 OK', [('Content-Type', 'text/plain')])
+        elif path.endswith(".css"):
+            start_response('200 OK', [('Content-Type', 'text/css')])
+        else:
+            start_response('200 OK', [('Content-Type', "text/xml")])
+        return [text]
+    except IOError:
+        resp = NotFound()
+        return resp(environ, start_response)
+
+def start(environ, start_response):
+        resp = Response(mako_template="start.mako",
+                        template_lookup=LOOKUP,
+                        headers=[])
+        argv = {
+            "service": rp_conf.SERVICE
+        }
+        return resp(environ, start_response, **argv)
+
+def oplist(environ, start_response):
+        resp = Response(mako_template="oplist.mako",
+                        template_lookup=LOOKUP,
+                        headers=[])
+        argv = {
+            "service": rp_conf.SERVICE
+        }
+        return resp(environ, start_response, **argv)
+
+def about(environ, start_response):
+        resp = Response(mako_template="about.mako",
+                        template_lookup=LOOKUP,
+                        headers=[])
+        argv = {
+        }
+        return resp(environ, start_response, **argv)
+
+def opbyuid(environ, start_response):
+        resp = Response(mako_template="opbyuid.mako",
+                        template_lookup=LOOKUP,
+                        headers=[])
+        argv = {
+        }
+        return resp(environ, start_response, **argv)
+
+def application(environ, start_response):
+    session = environ['beaker.session']
+    rpSession = RpSession(session)
+
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    if path == "robots.txt":
+        return static(environ, start_response, LOGGER, "static/robots.txt")
+
+    if path.startswith("static/"):
+        return static(environ, start_response, LOGGER, path)
+
+    query = parse_qs(environ["QUERY_STRING"])
+    for key, _dict in rp_conf.SERVICE.items():
+        if "opKey" in _dict and _dict["opKey"] == path:
+            func = getattr(rp_conf.SERVICE[key]["instance"], "callback")
+            return func(environ, SERVER_ENV, start_response, query, rpSession)
+
+    if path == "rp":
+        if "key" in query:
+            print "key"
+            key = query["key"][0]
+            if key in rp_conf.SERVICE:
+                func = getattr(rp_conf.SERVICE[key]["instance"], "begin")
+                return func(environ,SERVER_ENV, start_response, rpSession)
+
+        if "uid" in query:
+            print "uid"
+            _val = URINormalizer().normalize(query["uid"][0])
+            wf = WebFinger(httpd = Httpd())
+            link = wf.discovery_query(resource=_val)
+            #requests.get(url, verify=True)
+            md5 = hashlib.md5()
+            md5.update(link)
+            opkey = base64.b16encode(md5.digest())
+            kwargs = {'opKey': opkey,
+                      'description': 'OIDC server with discovery url: ' + link,
+                      'class': pyoidcOIC,
+                      'srv_discovery_url': link,
+                      'scope': ["openid", "profile", "email", "address", "phone"],
+                      'name': link}
+            rp_conf.SERVICE[opkey] = kwargs
+            rp_conf.SERVICE[opkey]["instance"] = pyoidcOIC(None, None, **kwargs)
+            func = getattr(rp_conf.SERVICE[opkey]["instance"], "begin")
+            return func(environ,SERVER_ENV, start_response, rpSession)
+
+
+
+    if path == "opbyuid":
+        return opbyuid(environ, start_response)
+    if path == "oplist":
+        return oplist(environ, start_response)
+    if path == "about":
+        return about(environ, start_response)
+    return start(environ, start_response)
+
+if __name__ == '__main__':
+    setup_server_env(rp_conf)
+    print "Starting test rp with configuration:"
+    for key, _dict in rp_conf.SERVICE.items():
+        _dict["instance"] = _dict["class"](**_dict)
+
+    session_opts = {
+        'session.type': 'memory',
+        'session.cookie_expires': True,
+        #'session.data_dir': './data',
+        'session.auto': True,
+        'session.timeout' : 900
+    }
+
+    SRV = wsgiserver.CherryPyWSGIServer(('0.0.0.0', rp_conf.PORT), SessionMiddleware(application, session_opts))
+
+    LOGGER.info("RP server starting listening on port:%s" % rp_conf.PORT)
+    print "RP server starting listening on port:%s" % rp_conf.PORT
+    try:
+        SRV.start()
+    except KeyboardInterrupt:
+        SRV.stop()
