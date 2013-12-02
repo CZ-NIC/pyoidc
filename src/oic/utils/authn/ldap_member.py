940cb0ca78e1f20c64ffde2b5157155664bfa2de
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-12-02 12:04:08 +0100

    PEP-8 compliance

--- src/oic/utils/authn/ldap_member.py
+++ src/oic/utils/authn/ldap_member.py
@@ -8,16 +8,17 @@ logger = logging.getLogger(__name__)
 
 class UserLDAPMemberValidation(UserInfoLDAP):
 
-    def __init__(self, verifyAttr=None, verifyAttrValid=None, **kwargs):
+    def __init__(self, verify_attr=None, verify_attr_valid=None, **kwargs):
         UserInfoLDAP.__init__(self, **kwargs)
-        self.verifyAttr = verifyAttr
-        self.verifyAttrValid = verifyAttrValid
+        self.verify_attr = verify_attr
+        self.verify_attr_valid = verify_attr_valid
 
     def __call__(self, userid, **kwargs):
         result = UserInfoLDAP.__call__(self, userid, None, False)
-        if self.verifyAttr in result:
-            for field in result[self.verifyAttr]:
-                if field in self.verifyAttrValid:
+        if self.verify_attr in result:
+            for field in result[self.verify_attr]:
+                if field in self.verify_attr_valid:
                     return True
-        logger.warning(userid + "tries to use the service with the values " + result)
+        logger.warning(userid + "tries to use the service with the values " +
+                       result)
         return False
