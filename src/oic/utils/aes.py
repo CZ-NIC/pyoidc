d920134e4f52b0bdb22ac191a643e096f2a5354d
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-12-03 14:51:56 +0100

    Use the same aes.py as pyjwkest

--- src/oic/utils/aes.py
+++ src/oic/utils/aes.py
@@ -12,6 +12,8 @@ POSTFIX_MODE = {
     "ecb": AES.MODE_CFB,
 }
 
+BLOCK_SIZE = 16
+
 
 def build_cipher(key, iv, alg="aes_128_cbc"):
     """
@@ -40,39 +42,68 @@ def build_cipher(key, iv, alg="aes_128_cbc"):
         raise Exception("Unsupported chaining mode")
 
 
-def encrypt(key, msg, iv=None, alg="aes_128_cbc"):
+def encrypt(key, msg, iv=None, alg="aes_128_cbc", padding="PKCS#7",
+            b64enc=True, block_size=BLOCK_SIZE):
     """
     :param key: The encryption key
     :param iv: init vector
     :param msg: Message to be encrypted
-    :return: The encrypted message base64 encoded
+    :param padding: Which padding that should be used
+    :param b64enc: Whether the result should be base64encoded
+    :param block_size: If PKCS#7 padding which block size to use
+    :return: The encrypted message
     """
 
+    if padding == "PKCS#7":
+        _block_size = block_size
+    elif padding == "PKCS#5":
+        _block_size = 8
+    else:
+        _block_size = 0
+
+    if _block_size:
+        plen = _block_size - (len(msg) % _block_size)
+        c = chr(plen)
+        msg += c*plen
+
     cipher, iv = build_cipher(key, iv, alg)
-    return b64encode(iv + cipher.encrypt(msg))
+    cmsg = iv + cipher.encrypt(msg)
+    if b64enc:
+        return b64encode(cmsg)
+    else:
+        return cmsg
 
 
-def decrypt(key, msg, iv=None):
+def decrypt(key, msg, iv=None, padding="PKCS#7", b64dec=True):
     """
     :param key: The encryption key
     :param iv: init vector
     :param msg: Base64 encoded message to be decrypted
     :return: The decrypted message
     """
-    data = b64decode(msg)
+    if b64dec:
+        data = b64decode(msg)
+    else:
+        data = msg
+
     _iv = data[:AES.block_size]
     if iv:
         assert iv == _iv
     cipher, iv = build_cipher(key, iv)
-    return cipher.decrypt(data)[AES.block_size:]
+    res = cipher.decrypt(data)[AES.block_size:]
+    if padding in ["PKCS#5", "PKCS#7"]:
+        res = res[:-ord(res[-1])]
+    return res
 
 if __name__ == "__main__":
     key_ = "1234523451234545"  # 16 byte key
-    # Message has to be multiple of 16 in length
-    msg_ = "ToBeOrNotTobe WS01234567"
+    # Iff padded the message doesn't have to be multiple of 16 in length
+    msg_ = "ToBeOrNotTobe W.S."
     iv_ = os.urandom(16)
     encrypted_msg = encrypt(key_, msg_, iv_)
-    print decrypt(key_, encrypted_msg, iv_)
+    txt = decrypt(key_, encrypted_msg, iv_)
+    assert txt == msg_
 
     encrypted_msg = encrypt(key_, msg_, 0)
-    print decrypt(key_, encrypted_msg, 0)
+    txt = decrypt(key_, encrypted_msg, 0)
+    assert txt == msg_
