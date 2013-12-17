f0d0d8b26b554175d423659e78c5f4e550660840
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-12-17 14:13:56 +0100

    Making a server stateless.

---
+++ src/oic/utils/stateless.py
@@ -0,0 +1,129 @@
+import time
+from oic.oic.message import SINGLE_REQUIRED_INT
+from oic.oauth2 import Message
+from oic.oauth2 import SINGLE_REQUIRED_STRING
+from oic.oauth2 import SINGLE_OPTIONAL_STRING
+from oic.utils.time_util import epoch_in_a_while
+
+__author__ = 'roland'
+
+
+class Content(Message):
+    c_param = {
+        "typ": SINGLE_REQUIRED_STRING,  # type of token
+        "sub": SINGLE_REQUIRED_STRING,  # Which subject that authenticated
+        "auz": SINGLE_OPTIONAL_STRING,  # Authorization information
+        "aud": SINGLE_OPTIONAL_STRING,  # The intended receiver
+        "val": SINGLE_REQUIRED_INT,     # Valid until
+        "ref": SINGLE_OPTIONAL_STRING   # Refresh token
+    }
+    c_allowed_values = {"type": ["code", "access", "refresh"]}
+
+
+class StateLess(object):
+    def __init__(self, keys, enc_alg, enc_method, grant_validity=300,
+                 access_validity=600, refresh_validity=0):
+        self.keys = keys
+        self.alg = enc_alg
+        self.enc = enc_method
+        self.validity = {"grant": grant_validity, "access": access_validity,
+                         "refresh": refresh_validity}
+        self.used_grants = []
+        self.revoked = []
+
+    def __getitem__(self, token):
+        """
+        :param token: authz grant code or refresh token
+        :return: information about the session
+        """
+        return Content().from_jwe(token, self.keys)
+
+    def get_token(self, cont):
+        return cont.to_jwe(self.keys, self.enc, self.alg)
+
+    def create_authz_session(self, sub, areq, **kwargs):
+        """
+
+        :param sub: Identifier for the user, this is the real identifier
+        :param areq: The AuthorizationRequest instance
+        :return: The session identifier, which is the database key
+        """
+        _cont = Content(typ="code", sub=sub, aud=areq["redirect_uri"],
+                        val=epoch_in_a_while(self.validity["grant"]))
+
+        #return _cont.to_jwe(self.keys, self.enc, self.alg)
+        return _cont
+
+    #def update_to_token(self, token=None, issue_refresh=True, **kwargs):
+    #    """
+    #
+    #    :param token: The access grant
+    #    :param issue_refresh: If a refresh token should be issued
+    #    :return: A new token
+    #    """
+    #    if token in self.used_grants:
+    #        raise Exception("Grant already used")
+    #
+    #    _cont = Content().from_jwe(token, self.keys)
+    #
+    #    try:
+    #        assert _cont["typ"] == "code"
+    #    except AssertionError:
+    #        raise Exception("Not a access grant")
+    #
+    #    self.used_grants.append(token)
+    #
+    #    _cont["typ"] = "access"
+    #    _cont["val"] = epoch_in_a_while(self.validity["access"])
+    #    if issue_refresh:
+    #        _c = Content(sub=_cont["sub"], aud=_cont["aud"], typ="refresh",
+    #                     val=epoch_in_a_while(self.validity["refresh"]))
+    #        _cont["ref"] = _c.to_jwe(self.keys, self.enc, self.alg)
+    #
+    #    return _cont.to_jwe(self.keys, self.enc, self.alg)
+
+    def upgrade_to_token(self, cont, issue_refresh=False):
+        cont["typ"] = "access"
+        cont["val"] = epoch_in_a_while(self.validity["access"])
+        if issue_refresh:
+            _c = Content(sub=cont["sub"], aud=cont["aud"], typ="refresh",
+                         val=epoch_in_a_while(self.validity["refresh"]))
+            cont["ref"] = _c.to_jwe(self.keys, self.enc, self.alg)
+
+        return cont
+
+    def refresh_token(self, rtoken):
+        # assert that it is a refresh token
+        _cont = Content().from_jwe(rtoken, self.keys)
+        try:
+            assert _cont["typ"] == "refresh"
+        except AssertionError:
+            raise Exception("Not a refresh token")
+
+    def is_expired(self, token):
+        _cont = Content().from_jwe(token, self.keys)
+        if _cont["val"] < time.time():
+            return True
+
+    def is_valid(self, token):
+        _cont = Content().from_jwe(token, self.keys)
+
+        if _cont["val"] >= time.time():
+            return False
+        if token in self.revoked:
+            return False
+        else:
+            return True
+
+    def is_revoked(self, token):
+        if token in self.revoked:
+            return True
+        else:
+            return False
+
+    def revoke_token(self, token):
+        # revokes either the refresh token or the access token
+        self.revoked.append(token)
+
+    def store_session(self, cont):
+        pass
---
+++ tests/test_stateless.py
@@ -0,0 +1,33 @@
+from oic.utils.stateless import StateLess
+
+__author__ = 'roland'
+
+
+def _eq(l1, l2):
+    return set(l1) == set(l2)
+
+
+def test_access_code():
+    keys = {"OCT": ["symmetric key123"]}
+    st = StateLess(keys, enc_alg="A128KW", enc_method="A128CBC-HS256")
+    con = st.create_authz_session("subject",
+                                  {"redirect_uri": "https://example.com"})
+    tok = st.encrypt(con)
+
+    _info = st[tok]
+    print _info
+    assert _eq(_info.keys(), ["typ", "aud", "val", "sub"])
+    assert _info["sub"] == "subject"
+    assert _info["typ"] == "code"
+    assert _info["aud"] == "https://example.com"
+
+
+def test_update_to_access_token():
+    keys = {"OCT": ["symmetric key123"]}
+    st = StateLess(keys, enc_alg="A128KW", enc_method="A128CBC-HS256")
+    tok = st.create_authz_session("subject",
+                                  {"redirect_uri": "https://example.com"})
+
+
+if __name__ == "__main__":
+    test_access_code()
