5f7f532fd225d291d48acd93218dcdf7388afc48
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-01-07 08:38:08 +0100

    I'm bad at changing version numbers, should have done this ages ago.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ __author__ = 'rohe0002'
 
 setup(
     name="oic",
-    version="0.3.0",
+    version="0.4.0beta",
     description="Python implementation of OAuth2 and OpenID Connect",
     author = "Roland Hedberg",
     author_email = "roland.hedberg@adm.umu.se",
