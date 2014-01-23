fb2ac83194aefe1dc965e9a2dca4725647e397ab
Hans Hörberg <hans.horberg@umu.se>
2014-01-23 10:53:41 +0100

    Corrections for rp1.

--- rp1/openidconnect/__init__.py
+++ rp1/openidconnect/__init__.py
@@ -133,7 +133,7 @@ class OpenIDConnect(Social):
             acr_value = session.getAcrValue(client.authorization_endpoint)
             try:
                 acr_values = client.provider_info[
-                    self.srv_discovery_url]["acr_values_supported"].split()
+                    self.srv_discovery_url]["acr_values_supported"]
                 session.setAcrvalues(acr_values)
             except:
                 pass
