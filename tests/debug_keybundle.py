e228c4a3bd5d52906f865c07c3fe883a2efc595b
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-04-24 13:32:19 +0200

    Removed debug files.

--- tests/debug_keybundle.py
+++
@@ -1,8 +0,0 @@
-from oic.utils.keyio import KeyBundle, rsa_load
-
-__author__ = 'rolandh'
-
-_key = rsa_load("../oc3/certs/mycert.key")
-
-KC_RSA = KeyBundle([{"key":_key, "kty":"rsa", "use":"ver"},
-                    {"key":_key, "kty":"rsa", "use":"sig"}])
