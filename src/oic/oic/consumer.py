f6dc157eff7d8075a649284ce571eff6dbe783c2
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-11-06 12:23:07 +0100

    Class variable should be defined in __init__

--- src/oic/oic/consumer.py
+++ src/oic/oic/consumer.py
@@ -158,6 +158,7 @@ class Consumer(Client):
         self.seed = ""
         self.nonce = ""
         self.request_filename = ""
+        self.request_uri = ""
         self.user_info = None
         self.registration_expires_at = 0
         self.secret_type = "Bearer"
