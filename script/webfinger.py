ee437b2b2a1cbdb505c1bb693f4223d7bf84f56c
Roland Hedberg <roland.hedberg@adm.umu.se>
2014-11-27 17:51:15 +0100

    Simple script to test webfinger support.

---
+++ script/webfinger.py
@@ -0,0 +1,9 @@
+import sys
+from oic.oauth2 import PBase
+from oic.utils.webfinger import WebFinger, OIC_ISSUER
+
+__author__ = 'roland'
+
+wf = WebFinger(OIC_ISSUER)
+wf.httpd = PBase()
+print wf.discovery_query(sys.argv[1])
