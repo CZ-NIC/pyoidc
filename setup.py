908d85f6325e363fcc79bafa164747409fc70f97
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-03-25 09:24:49 +0100

    remove python-ldap requirement. Need for a special use case but that can be dealt with there.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name="oic",
-    version="0.7.2",
+    version="0.7.3",
     description="Python implementation of OAuth2 and OpenID Connect",
     author="Roland Hedberg",
     author_email="roland.hedberg@umu.se",
