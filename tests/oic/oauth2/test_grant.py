df00b347df7f6800d061a2ffb2fb418a5d8fb94b
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-05 20:21:28 +0200

    Major refactor of test_oauth2.py. Extracted tests into separate files, grouped tests in classes. Fixed some bugs in the tests and removed all time.sleep in them.

---
+++ tests/oic/oauth2/test_grant.py
@@ -0,0 +1,109 @@
+# pylint: disable=missing-docstring,no-self-use
+
+from oic.oauth2.grant import Grant, Token
+from oic.oauth2.message import AuthorizationResponse, ErrorResponse, \
+    AccessTokenResponse
+from utils_for_tests import _eq  # pylint: disable=import-error
+
+ATR = AccessTokenResponse(access_token="2YotnFZFEjr1zCsicMWpAA",
+                          token_type="example",
+                          refresh_token="tGzv3JOkF0XG5Qx2TlKWIA",
+                          example_parameter="example_value",
+                          scope=["inner", "outer"])
+AR = AuthorizationResponse(code="code", state="state")
+
+
+class TestGrant(object):
+    def test_expiration_time(self):
+        grant = Grant()
+        assert grant.exp_in == 600
+        assert grant.grant_expiration_time == 0
+
+        grant = Grant(60)
+        assert grant.exp_in == 60
+
+        grant = Grant(-1, AR)
+        assert grant.grant_expiration_time != 0
+        assert not grant.is_valid()
+
+    def test_from_code(self):
+        grant = Grant.from_code(AR)
+
+        assert grant.code == "code"
+        assert grant.grant_expiration_time != 0
+
+    def test_add_code(self):
+        grant = Grant()
+        grant.add_code(AR)
+        assert grant.code == "code"
+
+    def test_update(self):
+        grant = Grant()
+        grant.update(AR)
+
+        assert grant.code == "code"
+
+    def test_set(self):
+        grant = Grant.from_code(AR)
+
+        assert grant.code == "code"
+
+    def test_add_token(self):
+        grant = Grant()
+        grant.update(ATR)
+
+        assert len(grant.tokens) == 1
+        token = grant.tokens[0]
+
+        assert token.access_token == "2YotnFZFEjr1zCsicMWpAA"
+        assert token.token_type == "example"
+        assert token.refresh_token == "tGzv3JOkF0XG5Qx2TlKWIA"
+
+    def test_update_with_error_resp(self):
+        err = ErrorResponse(error="invalid_request")
+        grant = Grant()
+        grant.update(err)
+
+        assert len(grant.tokens) == 0
+
+
+class TestToken(object):
+    def test_access_token(self):
+        atr = AccessTokenResponse(access_token="2YotnFZFEjr1zCsicMWpAA",
+                                  token_type="example", expires_in=-1,
+                                  refresh_token="tGzv3JOkF0XG5Qx2TlKWIA",
+                                  example_parameter="example_value",
+                                  xscope=["inner", "outer"])
+        token = Token(atr)
+        assert _eq(token.keys(), ['token_expiration_time', 'access_token',
+                                  'expires_in', 'example_parameter',
+                                  'token_type',
+                                  'xscope', 'refresh_token', 'scope',
+                                  'replaced'])
+
+        assert token.access_token == "2YotnFZFEjr1zCsicMWpAA"
+        assert token.token_type == "example"
+        assert token.refresh_token == "tGzv3JOkF0XG5Qx2TlKWIA"
+        assert token.example_parameter == "example_value"
+        assert token.xscope == ["inner", "outer"]
+        assert token.token_expiration_time != 0
+        assert not token.is_valid()
+
+
+def test_grant_access_token():
+    resp = AuthorizationResponse(code="code", state="state")
+    grant = Grant()
+    grant.add_code(resp)
+
+    atr = AccessTokenResponse(access_token="2YotnFZFEjr1zCsicMWpAA",
+                              token_type="example",
+                              refresh_token="tGzv3JOkF0XG5Qx2TlKWIA",
+                              example_parameter="example_value",
+                              scope=["inner", "outer"])
+
+    grant.add_token(atr)
+    assert len(grant.tokens) == 1
+    token = grant.tokens[0]
+    assert token.is_valid() is True
+
+    assert str(grant) != ""
