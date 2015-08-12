84e16c5264cb2fde34293168766dfbc10d6ce9d9
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-12 11:50:43 +0200

    Updated test_shelve_wrapper.py.

--- tests/oic/utils/test_shelve_wrapper.py
+++ tests/oic/utils/test_shelve_wrapper.py
@@ -1,143 +1,71 @@
-from builtins import range
+# pylint: disable=missing-docstring,no-self-use,redefined-outer-name
+
 import os
-from oic.utils.shelve_wrapper import open
+
 import pytest
+
+from oic.utils import shelve_wrapper
+from utils_for_tests import _eq  # pylint: disable=import-error
 
 __author__ = 'mathiashedstrom'
 
-DB_NAMNE = "test_db_shelve_wrapper"
+VALUES = {"key_1": "val_1", "key_2": "val_2", "key_3": "val_3"}
 
-def teardown_module(module):
-    _clear_db()
 
 @pytest.fixture
-def db():
-    _clear_db()
-    return open(DB_NAMNE, writeback=True)
-
-def test_keys(db):
-    key_1 = "key_1"
-    key_2 = "key_2"
-    key_3 = "key_3"
-
-    db[key_1] = "foo"
-    db[key_2] = "foo"
-    db[key_3] = "foo"
-
-    key_list = list()
-    key_list.append(key_1)
-    key_list.append(key_2)
-    key_list.append(key_3)
-
-    keys = list(db.keys())
-
-    for k in keys:
-        assert k in key_list
-
-def test_has_keys(db):
-    key_1 = "key_1"
-    key_2 = "key_2"
-    key_3 = "key_3"
-
-    db[key_1] = "foo"
-    db[key_2] = "foo"
-    db[key_3] = "foo"
-
-    key_list = list()
-    key_list.append(key_1)
-    key_list.append(key_2)
-    key_list.append(key_3)
-
-    for k in key_list:
-        assert db.has_key(k)
-
-    assert not db.has_key("NO_KEY")
-
-def test_contains(db):
-    key_1 = "key_1"
-    key_2 = "key_2"
-    key_3 = "key_3"
-
-    db[key_1] = "foo"
-    db[key_2] = "foo"
-    db[key_3] = "foo"
-
-    key_list = list()
-    key_list.append(key_1)
-    key_list.append(key_2)
-    key_list.append(key_3)
-
-    for k in key_list:
-        assert k in db
-
-    assert "NO_KEY" not in db
-
-def test_get(db):
-    key_list = list()
-    value_list = list()
-
-    for i in range(3):
-        key = "key_{}".format(i)
-        value = "value_{}".format(i)
-        key_list.append(key)
-        value_list.append(value)
-        db[key] = value
-
-    for index, key in enumerate(key_list):
-        assert db.get(key) == value_list[index]
-
-    assert db.get("NO_KEY") == None
-
-def test_getitem(db):
-    key_list = list()
-    value_list = list()
-
-    for i in range(3):
-        key = "key_{}".format(i)
-        value = "value_{}".format(i)
-        key_list.append(key)
-        value_list.append(value)
-        db[key] = value
-
-    for index, key in enumerate(key_list):
-        assert db[key] == value_list[index]
-
-    with pytest.raises(KeyError):
-        db["NO_KEY"]
-
-def test_delitem(db):
-    key_list = list()
-    value_list = list()
-
-    for i in range(3):
-        key = "key_{}".format(i)
-        value = "value_{}".format(i)
-        key_list.append(key)
-        value_list.append(value)
-        db[key] = value
-
-    del db[key_list[0]]
-
-    with pytest.raises(KeyError):
-        db[key_list[0]]
-
-    for index in range(1, len(key_list)):
-        assert db[key_list[index]] == value_list[index]
+def db(tmpdir):
+    return shelve_wrapper.open(
+        os.path.join(tmpdir.strpath, "test_db_shelve_wrapper"), writeback=True)
 
 
-def test_len(db):
-    assert len(db) == 0
-    lenght = 4
-    for i in range(lenght):
-        db["key_{}".format(i)] = "foo"
-    assert len(db) == lenght
+@pytest.fixture
+def populated_db(db):
+    for k, v in VALUES.items():
+        db[k] = v
 
-def _clear_db():
-    extensions = ["db", "bak", "dat", "dir"]
-    for e in extensions:
-        try:
-            os.remove("{db_name}.{extension}".format(db_name=DB_NAMNE, extension=e))
-        except:
-            pass
+    return db
 
-#_clear_db()
+
+class TestShelfWrapper(object):
+    def test_keys(self, populated_db):
+        assert _eq(populated_db.keys(), VALUES.keys())
+
+    def test_has_keys(self, populated_db):
+        for k in VALUES.keys():
+            assert populated_db.has_key(k)
+
+        assert not populated_db.has_key("NO_KEY")
+
+    def test_contains(self, populated_db):
+        for k in VALUES.keys():
+            assert k in populated_db
+
+        assert "NO_KEY" not in populated_db
+
+    def test_get(self, populated_db):
+        for k, v in VALUES.items():
+            assert populated_db.get(k) == v
+
+        assert populated_db.get("NO_KEY") is None
+
+    def test_getitem(self, populated_db):
+        for k, v in VALUES.items():
+            assert populated_db[k] == v
+
+        with pytest.raises(KeyError):
+            populated_db["NO_KEY"]  # pylint: disable=pointless-statement
+
+    def test_delitem(self, populated_db):
+        key = list(VALUES.keys())[0]
+        del populated_db[key]
+
+        with pytest.raises(KeyError):
+            populated_db[key]  # pylint: disable=pointless-statement
+
+    def test_len(self, db):
+        assert len(db) == 0
+
+        length = 4
+        for i in range(length):
+            db["key_{}".format(i)] = "foo"
+        assert len(db) == length
