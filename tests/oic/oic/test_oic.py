5daf284327a3371a29941107db655290b8103a7a
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-12 16:24:21 +0200

    Use absolute path for file.

--- tests/oic/oic/test_oic.py
+++ tests/oic/oic/test_oic.py
@@ -40,8 +40,9 @@ KC_SYM_S = KeyBundle(
     {"kty": "oct", "key": "abcdefghijklmnop".encode("utf-8"), "use": "sig",
      "alg": "HS256"})
 
-BASE_PATH = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir,
-                         "data/keys")
+BASE_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir,
+                 "data/keys"))
 _key = rsa_load(os.path.join(BASE_PATH, "rsa.key"))
 KC_RSA = KeyBundle({"key": _key, "kty": "RSA", "use": "sig"})
 
