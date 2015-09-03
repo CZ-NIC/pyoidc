bfbd5fb757e7331af2e908fb942e10f9a252977c
Roland Hedberg <roland.hedberg@adm.umu.se>
2015-09-03 07:52:53 +0200

    Publish python version support.

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
+
 # Python 2.7 and later ship with importlib and argparse
 if sys.version_info[0] == 2 and sys.version_info[1] == 6:
     extra_install_requires = ["importlib", "argparse"]
@@ -58,22 +59,24 @@ setup(
     packages=["oic", "oic/oauth2", "oic/oic", "oic/utils", "oic/utils/authn",
               "oic/utils/userinfo"],
     package_dir={"": "src"},
-    classifiers=["Development Status :: 4 - Beta",
-                 "License :: OSI Approved :: Apache Software License",
-                 "Topic :: Software Development :: Libraries :: Python "
-                 "Modules"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
     extras_require={
         'develop': ["cherrypy==3.2.4"],
     },
     install_requires=[
-        "requests",
-        "pycrypto>=2.6.1",
-        "pyjwkest>=1.0.3",
-        "mako",
-        "beaker",
-        "alabaster",
-        "pyOpenSSL",
-        "six"] + extra_install_requires,
+                         "requests",
+                         "pycrypto>=2.6.1",
+                         "pyjwkest>=1.0.3",
+                         "mako",
+                         "beaker",
+                         "alabaster",
+                         "pyOpenSSL",
+                         "six"] + extra_install_requires,
     zip_safe=False,
     cmdclass={'test': PyTest},
 )
