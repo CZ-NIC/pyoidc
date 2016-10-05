9d6e0ccd5c01bbce10b3cad348d0dfeba59d0962
Roland Hedberg <roland@catalogix.se>
2016-10-05 13:03:30 +0200

    Release version

--- src/oic/__init__.py
+++ src/oic/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
     import random as rnd
 
 __author__ = 'rohe0002'
-__version__ = '0.9.0.1'
+__version__ = '0.9.1.0'
 
 
 OIDCONF_PATTERN = "%s/.well-known/openid-configuration"
