e2a2db8c3ace5637208e41412cf39381362887df
roland <roland@catalogix.se>
2016-11-02 15:01:46 -0700

    This mismatch is not really an error more of a warning.

--- src/oic/oic/__init__.py
+++ src/oic/oic/__init__.py
@@ -986,7 +986,11 @@ class Client(oauth2.Client):
 
                 claims = [value for value, src in
                           userinfo["_claim_names"].items() if src == csrc]
-                assert claims == _uinfo.keys()
+
+                if set(claims) != set(list(_uinfo.keys())):
+                    logger.warning(
+                        "Claims from claim source doesn't match what's in "
+                        "the userinfo")
 
                 for key, vals in _uinfo.items():
                     userinfo[key] = vals
