c5e3539a1e2a1bde91193e64b7b6d66a33ffd4cf
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2017-03-03 08:50:13 +0100

    remove "oic/federation" from packages provided in setup.py
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     license="Apache 2.0",
     url='https://github.com/rohe/pyoidc',
     packages=["oic", "oic/oauth2", "oic/oic", "oic/utils",
-              "oic/utils/authn", 'oic/federation',
+              "oic/utils/authn",
               "oic/utils/userinfo", 'oic/utils/rp', 'oic/extension'],
               # 'oic/v2'],
     package_dir={"": "src"},
