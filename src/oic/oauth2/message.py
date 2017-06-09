cce0c181732db24d66361e75d594b760c7252a74
Roland Hedberg <roland@catalogix.se>
2017-06-09 08:57:09 +0200

    Made pylama happy.

--- src/oic/oauth2/message.py
+++ src/oic/oauth2/message.py
@@ -804,13 +804,16 @@ class Message(MutableMapping):
 
     def to_jwe(self, keys, enc, alg, lev=0):
         """
+        Place the information in this instance in a JSON object. Make that
+        JSON object the body of a JWT. Then encrypt that JWT using the
+        specified algorithms and the given keys. Return the encrypted JWT.
 
         :param keys: Dictionary, keys are key type and key is the value or
         simple list.
         :param enc: Content Encryption Algorithm
         :param alg: Key Management Algorithm
         :param lev: Used for JSON construction
-        :return: An encrypted JWT. If encryption failed an exception will be 
+        :return: An encrypted JWT. If encryption failed an exception will be
         raised.
         """
         if isinstance(keys, dict):
@@ -821,11 +824,13 @@ class Message(MutableMapping):
 
     def from_jwe(self, msg, keys):
         """
-        
-        :param msg: An encrypted JWT 
+        Decrypt an encrypted JWT and load the JSON object that was the body
+        of the JWT into this object.
+
+        :param msg: An encrypted JWT
         :param keys: Dictionary, keys are key type and key is the value or
         simple list.
-        :return: The decrypted message. If decryption failed an exception 
+        :return: The decrypted message. If decryption failed an exception
         will be raised.
         """
         if isinstance(keys, dict):
