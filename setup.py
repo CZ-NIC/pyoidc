e4ef31fb9442c498d07cc05a6d12aa4c78520f07
Luke Murphy <lukewm@riseup.net>
2017-08-06 16:00:30 +0200

    Add other tag for Python 3.6.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     extras_require={
         'develop': ["cherrypy==3.2.4"],
