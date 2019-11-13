2713044282ddc3f19a8d332753e216e793dfa466
Tomas Pazderka <tomas.pazderka@oidf.org>
2019-11-13 22:56:29 +0100

    Run integration tests in parallel

--- docker/integration_tests/run.sh
+++ docker/integration_tests/run.sh
@@ -14,38 +14,16 @@ export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 cd /root/oidc-provider-conformance-tests
 export ISSUER=https://op:4433
-echo "---Running oidc-provider-conformance-tests 'code'---"
-npm run code || exit
-echo "---Running oidc-provider-conformance-tests 'id_token'---"
-npm run id_token || exit
-echo "---Running oidc-provider-conformance-tests 'id_token+token'---"
-npm run id_token+token || exit
-echo "---Running oidc-provider-conformance-tests 'code+id_token'---"
-npm run code+id_token || exit
-echo "---Running oidc-provider-conformance-tests 'code+id_token+token'---"
-npm run code+id_token+token || exit
-echo "---Running oidc-provider-conformance-tests 'code+token'---"
-npm run code+token || exit
+echo "---Running oidc-provider-conformance-tests 'code' and 'id_token'---"
+concurrently -- "npm:code" "npm:id_token" || exit
+echo "---Running oidc-provider-conformance-tests 'id_token+token' and 'code+id_token'---"
+concurrently -- "npm:id_token+token" "npm:code+id_token" || exit
+echo "---Running oidc-provider-conformance-tests 'code+id_token+token' and 'code+token'---"
+concurrently -- "npm:code+id_token+token" "npm:code+token" || exit
 
 cd /root/openid-client-conformance-tests
 export ISSUER=https://rp-test:8080
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-echo "---Running openid-client-conformance-tests 'non-profile'---"
-npm run non-profile || exit
-echo "---Running openid-client-conformance-tests 'basic'---"
-npm run basic || exit
-echo "---Running openid-client-conformance-tests 'config'---"
-npm run config || exit
-echo "---Running openid-client-conformance-tests 'dynamic'---"
-npm run dynamic || exit
-echo "---Running openid-client-conformance-tests 'id_token-implicit'---"
-npm run id_token-implicit || exit
-echo "---Running openid-client-conformance-tests 'id_token+token-implicit'---"
-npm run id_token+token-implicit || exit
-echo "---Running openid-client-conformance-tests 'code+id_token-hybrid'---"
-npm run code+id_token-hybrid || exit
-echo "---Running openid-client-conformance-tests 'code+token-hybrid'---"
-npm run code+token-hybrid || exit
-echo "---Running openid-client-conformance-tests 'code+id_token+token-hybrid'---"
-npm run code+id_token+token-hybrid || exit
+echo "---Running openid-client-conformance-tests---"
+npm run test || exit
 echo "---FINISHED---"
