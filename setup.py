20ae5934ea7567e8ec8eac18189032ad9683c2d7
Michael Schlenker <MichaelSchlenker@gmx.net>
2025-06-03 10:55:31 +0200

    chore: trigger rebuild

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setup(
     long_description=open("README.rst", encoding="utf-8").read(),
     author="Roland Hedberg",
     author_email="roland@catalogix.se",
-    license="Apache-2.0",
     license_files=["LICENSE.txt"],
+    license="Apache-2.0",
     url="https://github.com/CZ-NIC/pyoidc/",
     packages=[
         "oic",
